3bc544680b690ee30562edb78f118ae8a7a6140f
Blerand Gashi <blerandgashi15@gmail.com>
2025-07-05 02:57:47 +0200

    starting off

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+let deckId
 
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
 
-let s = 10;
-let multiply = 1;
-
-for(let i = 1; i <= s; i++){
-  multiply *= i
+        document.querySelector("#draw-cards").disabled = false;
 }
-console.log(multiply);
+
+document.querySelector(".newDeck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        })
+})
