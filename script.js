47d209390d05d7be96a39b961a042911e9036087
Blerand Gashi <blerandgashi15@gmail.com>
2025-07-07 03:32:19 +0200

    added a feature to show the winner

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const cardsContainer = document.querySelector(".imgContainer");
 const newDeckBtn = document.querySelector(".newDeck");
 const drawBtn = document.querySelector("#draw-cards");
 
+const winnerString = document.querySelector("h2");
+
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
       .then(res => res.json())
@@ -21,15 +23,33 @@ drawBtn.addEventListener("click", () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
       .then(res => res.json())
       .then(data => {
-        console.log(data.cards[0].image);
-        
-        cardsContainer.children[0].innerHTML =
-        `
-          <img src=${data.cards[0].image} class="card"/>
-        `
-        cardsContainer.children[1].innerHTML = 
-        `
-          <img src=${data.cards[1].image} class="card"/>
-        `
+          //<img src=${data.cards[0].image} class="card"/>
+          cardsContainer.children[0].innerHTML = 
+          `
+            <img src=${data.cards[0].image} class="card"/>
+          `
+          cardsContainer.children[1].innerHTML = 
+          `
+            <img src=${data.cards[1].image} class="card"/>
+          `
+          console.log(data);
+
+          const winner = highestScore(data.cards[0].value, data.cards[1].value);
+          winnerString.textContent = winner
+          
       })
 })
+
+function highestScore(card1, card2){
+  const valueArr = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING', 'ACE']
+  const card1Value = valueArr.indexOf(card1);
+  const card2Value = valueArr.indexOf(card2);
+
+  if (card1Value > card2Value) {
+    return "Card 1 won"
+  }else if (card1Value < card2Value) {
+    return "Card 2 won"
+  }else{
+    return "War"
+  }
+}
